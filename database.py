bfbbec63c507543dfb333cb2a3894f3507ebe60a
Mr.Benício <lbeniciosilva@gmail.com>
2023-11-27 14:19:52 -0300

    BD commit

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# Inicialização do objeto SQLAlchemy
+# InicializaÃ§Ã£o do objeto SQLAlchemy
 db = SQLAlchemy()
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-from database import db # Importa a inst‚ncia do SQLAlchemy
+from database import db # Importa a inst√¢ncia do SQLAlchemy
 
 class Inscritos(db.Model): # Define um modelo de dados chamado "Inscritos"
     __tablename__ = 'Inscritos' # Nome da tabela no banco de dados
-    id = db.Column(db.Integer, primary_key=True) # Coluna 'id' do tipo inteiro e chave prim·ria
-    email = db.Column(db.String(120), unique=True, nullable=False)  # Coluna 'email' do tipo string, ˙nico e n„o nulo
+    id = db.Column(db.Integer, primary_key=True) # Coluna 'id' do tipo inteiro e chave prim√°ria
+    email = db.Column(db.String(120), unique=True, nullable=False)  # Coluna 'email' do tipo string, √∫nico e n√£o nulo
 
-    def __init__(self, email): # MÈtodo inicializador para a classe Inscritos
-        self.email = email # Define o atributo 'email' ao criar uma inst‚ncia do modelo
+    def __init__(self, email): # M√©todo inicializador para a classe Inscritos
+        self.email = email # Define o atributo 'email' ao criar uma inst√¢ncia do modelo
