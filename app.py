03ca787c285e87e3459de302e402f1e461ed34da
Mr.Benício <lbeniciosilva@gmail.com>
2023-11-30 01:40:24 -0300

    App commit

--- app.py
+++ app.py
@@ -1,70 +1,70 @@
 # Importação das bibliotecas necessárias
-from flask import Flask, render_template, request, jsonify
-from validate_email_address import validate_email
-from models import Inscritos
-from database import db
-from flask_migrate import Migrate
-import dns.resolver
+from flask import Flask, render_template, request, jsonify  # Importa classes e métodos do Flask
+from validate_email_address import validate_email  # Importa função de validação de e-mail
+from models import Inscritos  # Importa o modelo de dados Inscritos
+from database import db  # Importa a instância do SQLAlchemy
+from flask_migrate import Migrate  # Importa a extensão Flask-Migrate para migrações de banco de dados
+import dns.resolver  # Importa a biblioteca para resolução de DNS
 
 # Configuração inicial da aplicação Flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'minha chave'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BD_SITE.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-migrate = Migrate(app, db)
+app = Flask(__name__)  # Inicializa a aplicação Flask
+app.config['SECRET_KEY'] = 'minha chave'  # Define a chave secreta para segurança
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BD_SITE.sqlite'  # Configura o banco de dados SQLite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desabilita o rastreamento de modificações no SQLAlchemy
+db.init_app(app)  # Inicializa a aplicação Flask com o SQLAlchemy
+migrate = Migrate(app, db)  # Configura a extensão Flask-Migrate para gerenciar migrações
 
 # Função para validar o formato do e-mail
 def validar_email(email):
-    is_valid = validate_email(email)
+    is_valid = validate_email(email)  # Valida o formato do e-mail usando a função importada
     return is_valid
 
 # Função para validar o domínio do e-mail
 def validar_dominio(email):
-    domain = email.split('@')[1]
+    domain = email.split('@')[1]  # Obtém o domínio do e-mail
     try:
         # Consulta de registros MX para o domínio
         mx_records = dns.resolver.resolve(domain, 'MX')
-        return bool(mx_records)
+        return bool(mx_records)  # Retorna True se houver registros MX para o domínio
     except dns.resolver.NoAnswer:
-        return False
+        return False  # Retorna False se não houver resposta
     except dns.resolver.NXDOMAIN:
-        return False
+        return False  # Retorna False se o domínio não existir
 
 # Rota para lidar com a inscrição por meio de requisições POST
 @app.route('/inscrever', methods=['POST'])
 def inscrever():
-    if request.method == 'POST':
-        email = request.form['email']
+    if request.method == 'POST':  # Verifica se é uma requisição POST
+        email = request.form['email']  # Obtém o e-mail do formulário
 
-        # Verifica se o e-mail e o domínio são válidos
+        # Verifica se o e-mail e o domínio são válidos usando as funções de validação
         if not validar_email(email) or not validar_dominio(email):
-            return jsonify({'message': 'Endereço de e-mail inválido ou domínio não válido.'}), 400
+            return jsonify({'message': 'Endereço de e-mail inválido ou domínio não válido.'}), 400  # Retorna erro 400 se o e-mail não for válido
 
-        # Verifica se o e-mail já está registrado
+        # Verifica se o e-mail já está registrado no banco de dados
         existente = Inscritos.query.filter_by(email=email).first()
         if existente:
-            return jsonify({'message': 'Este e-mail já está registrado.'}), 409
+            return jsonify({'message': 'Este e-mail já está registrado.'}), 409  # Retorna erro 409 se o e-mail já estiver registrado
         else:
             # Registra o e-mail se não estiver previamente cadastrado
             inscrito = Inscritos(email=email)
-            db.session.add(inscrito)
-            db.session.commit()
-            return jsonify({'message': 'Inscrição realizada com sucesso!'}), 201
+            db.session.add(inscrito)  # Adiciona o e-mail à sessão do banco de dados
+            db.session.commit()  # Confirma a adição no banco de dados
+            return jsonify({'message': 'Inscrição realizada com sucesso!'}), 201  # Retorna mensagem de sucesso
 
 # Rotas para as páginas principais do site
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  # Renderiza o template da página inicial
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html')
+    return render_template('sobre.html')  # Renderiza o template da página "Sobre nós"
 
 @app.route('/contato')
 def contato():
-    return render_template('contato.html')
+    return render_template('contato.html')  # Renderiza o template da página de contato
 
 # Inicializa a aplicação Flask
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Executa o aplicativo Flask em modo de depuração
